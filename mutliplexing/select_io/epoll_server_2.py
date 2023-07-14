92a6972c2ae86ce0788805daa748b98b1f514325
psykomal <bkomalsai@gmail.com>
2023-07-14 18:42:13 +0530

    select server 8080

--- mutliplexing/select_io/epoll_server_2.py
+++ mutliplexing/select_io/epoll_server_2.py
@@ -8,7 +8,7 @@ EPOLLEXCLUSIVE = 1<<28
 if True:
     sd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sd.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sd.bind(('127.0.0.1', 1024))
+    sd.bind(('127.0.0.1', 8080))
     sd.listen(10)
     sd.setblocking(False)
     for i in range(3):
