df0e7d809cfe4b1219e4bbe79dbfffe2d29e0bfb
psykomal <bkomalsai@gmail.com>
2023-07-14 16:17:27 +0530

    epoll changes

--- mutliplexing/select_io/select_server.py
+++ mutliplexing/select_io/select_server.py
@@ -13,21 +13,22 @@ def child_process(epoll, sock):
 
 if __name__ == '__main__':
 
-  # Create and bind socket
-  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  sock.bind(('127.0.0.1', 8080))
-  sock.listen(5)
-  sock.setblocking(False)
+    # Create and bind socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind(('127.0.0.1', 8080))
+    sock.listen(5)
+    sock.setblocking(False)
 
-  # Create epoll instance
-  epoll = select.epoll()
-  epoll.register(sock.fileno(), select.EPOLLIN)
+    # Create epoll instance
+    epoll = select.epoll()
+    epoll.register(sock.fileno(), select.EPOLLIN)
 
-  # Fork child processes
-  for i in range(4):
-    pid = os.fork()
-    if pid == 0:
-      child_process(epoll, sock)
+    # Fork child processes
+    for i in range(4):
+        pid = os.fork()
+        if pid == 0:
+            child_process(epoll, sock)
 
-  while True:
-    pass
+    while True:
+        pass
